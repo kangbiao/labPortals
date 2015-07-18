745d61bd35a893ac7765106f76529bf444e33a20
kangbiao <1316278289@qq.com>
2015-07-18 13:38:06 +0800

    测试

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '%|P1]wkYtDu:ap_Gs7FzM6*yq?"&~`=)9WKcHB[-'); //加密KEY
-define('UC_DB_DSN', 'mysql://root:kb123456@127.0.0.1:3306/onethink'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:siyecao1A@127.0.0.1:3306/onethink'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'ot_'); // 数据表前缀，使用Model方式调用API必须配置此项
